c9373d105d37255749f4d2a1300e767bf32e51dd
anisiomilani <anisio.milani@estudante.ufjf.br>
2025-07-25 15:52:09 -0300

    covnersor malha aberta funcionando

---
+++ shared_vars.h
@@ -0,0 +1,25 @@
+/*
+ * shared_vars.h
+ *
+ *  Created on: 4 de jul de 2025
+ *      Author: Prof. Guilherme Márcio Soares
+ */
+
+#ifndef SHARED_VARS_H_
+#define SHARED_VARS_H_
+
+extern float fVal;
+
+//extern volatile float adcVoltage;
+//extern volatile uint32_t cmp_Value;
+//extern volatile float g_duty_cycle;
+//extern uint16_t adcResult;
+//extern float fResult;
+
+//extern float REF;
+
+//extern float x[3];
+//extern float y[3];
+
+
+#endif /* SHARED_VARS_H_ */
