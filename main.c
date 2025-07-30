c2c22f048fdfaa11010c0f9eafba1f63ca92b379
anisiomilani <anisio.milani@estudante.ufjf.br>
2025-07-29 21:49:58 -0300

    falta apenas resetar automatica

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "board.h"
 #include "math.h"
 
+
 /*
  *
  * #define ADC0_BASE ADCA_BASE é o pino AA0
@@ -13,23 +14,24 @@
 
 // Parte de compartilhamento de memória
 
-//#pragma DATA_SECTION(adcVoltage, "CPUToCla1MsgRAM")
-//extern volatile float adcVoltage;
-
-
 #pragma DATA_SECTION(fVal,"CpuToCla1MsgRAM");
 float fVal;
+
 #pragma DATA_SECTION(fResult,"Cla1ToCpuMsgRAM");
 float fResult;
+
 #pragma DATA_SECTION(adcVoltage,"Cla1ToCpuMsgRAM");
 volatile float adcVoltage;
+
 #pragma DATA_SECTION(REF,"Cla1ToCpuMsgRAM");
 float REF = 8.0f;
 
 
 // VREF é a tensão de referência do DAC/ADC
 
-#define norm_DAC 4095.0f/18.0f
+#define norm_DAC 4095.0f/12.0f
+
+#define norm_DAC_il 4095.0f/1.2f
 //#define norm_ADC  18.0f/4095.0F
 
 // varaveis criadas para  PWM
@@ -60,19 +62,19 @@ volatile uint32_t cmp_Value;
 
 volatile float32_t g_vout_sim = 0.0f;        // Tensão de saída simulada
 volatile float32_t g_il_sim = 0.0f;          // Corrente no indutor simulada
-volatile uint32_t g_step_counter = 0;  // Contador de passos dentro do ciclo PWM
+volatile uint32_t g_step_counter = 0;        // Contador de passos dentro do ciclo PWM
 volatile bool g_switch_on = false;           // Estado da chave (true = ligada)
-volatile bool g_new_step_ready = false;     // Flag para novo passo de simulação
+volatile bool g_new_step_ready = false;      // Flag para novo passo de simulação
 volatile float g_duty_cycle = 0.5f;          // Razão cíclica (entre 0 e 1)
 
-__interrupt void INT_myGPIO0_XINT_ISR(void);
-__interrupt void INT_myCPUTIMER0_ISR(void);
+uint16_t dacVal,dacVal_il;
 
 void main(void)
 {
-    uint16_t dacVal;
+   // uint16_t dacVal,dacVal_il;
     float32_t v_l, i_c;
     // Inicialização dos periféricos
+
     Device_init();
     Interrupt_initModule();
     Interrupt_initVectorTable();
@@ -88,9 +90,8 @@ void main(void)
     while (1)
     {
 
-
-      //  cmp_Value = (uint32_t) (g_duty_cycle * ePwm_TimeBase);
-      //  EPWM_setCounterCompareValue(EPWM0_BASE, EPWM_COUNTER_COMPARE_A, cmp_Value);
+     //  cmp_Value = (uint32_t) (g_duty_cycle * ePwm_TimeBase);
+     //  EPWM_setCounterCompareValue(EPWM0_BASE, EPWM_COUNTER_COMPARE_A, cmp_Value);
      //   ePwm_curDuty = EPWM_getCounterCompareValue(EPWM0_BASE, EPWM_COUNTER_COMPARE_A);
 
         if (g_new_step_ready)
@@ -117,7 +118,20 @@ void main(void)
 
            dacVal = (dacVal > 4095) ? 4095 :  dacVal;
 
-            DAC_setShadowValue(DAC0_BASE, dacVal);
+           DAC_setShadowValue(DAC0_BASE, dacVal);
+
+
+
+            dacVal_il = (uint16_t) ((g_il_sim * norm_DAC_il));
+
+          // dacVal_il = (uint16_t) ((dacVal*INV_R_LOAD));
+
+
+            dacVal_il = (dacVal_il > 4095) ? 4095 :  dacVal_il;
+
+
+            DAC_setShadowValue(DAC1_BASE, dacVal_il);
+
 
         }
     }
@@ -146,30 +160,3 @@ __interrupt void INT_myCPUTIMER0_ISR(void)
     // Libera nova interrupção
     Interrupt_clearACKGroup(INT_myCPUTIMER0_INTERRUPT_ACK_GROUP);
 }
-
-/*
-__interrupt void INT_ADC0_1_ISR(void)
-{
-    // Dispara conversão ADC no canal 0 (AA0)
-    // Lê valor convertido do ADC
-    uint16_t adcResult;
-
-    adcResult = ADC_readResult(ADCARESULT_BASE, ADC0_SOC0);
-
-    // Converte adcResult para volts (se quiser)
-    adcVoltage = ((float) (adcResult*norm_ADC));
-
-    fVal = adcVoltage;
-
-    ADC_clearInterruptStatus(ADC0_BASE, ADC_INT_NUMBER1);
-    Interrupt_clearACKGroup(INT_ADC0_1_INTERRUPT_ACK_GROUP);
-
-}
-
-__interrupt void cla1Isr1(void)
-{
-  EPWM_setCounterCompareValue(EPWM0_BASE, EPWM_COUNTER_COMPARE_A, duty_cmp);
-
-  Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP11);
-}
-*/
